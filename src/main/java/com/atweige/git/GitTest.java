7dfa2e45701c8c200df1e4324bb142d58ffd1645
weige <630963258@qq.com>
2022-06-14 17:16:31 +0800

    first commit

---
+++ src/main/java/com/atweige/git/GitTest.java
@@ -0,0 +1,10 @@
+package com.atweige.git;
+
+/**
+ * @date 2022/6/14 - 17:10
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
